023de40abd9fa45c6ac826c875a656ec93a339f4
Kjell Heinsen <ich@kjell-heinsen.de>
2025-07-08 18:06:11 +0200

    JWT Function

--- classes/jwt.php
+++ classes/jwt.php
@@ -6,6 +6,36 @@ class jwt{
 
 
     /**
+     * Verifiziert die Signatur eines JWT
+     *
+     * @param string $input Daten, die signiert wurden
+     * @param string $signature Zu überprüfende Signatur
+     * @param string $key Schlüssel für die Verifikation
+     * @param string $algorithm Verwendeter Algorithmus
+     * @return bool True wenn die Signatur gültig ist
+     * @throws Exception Bei Fehler in der Verifikation
+     */
+    private static function verify(string $input, string $signature, string $key, string $algorithm): bool {
+        list($function, $algo) = self::$supportedAlgorithms[$algorithm];
+
+        switch ($function) {
+            case 'hash_hmac':
+                $hash = hash_hmac($algo, $input, $key, true);
+                return hash_equals($hash, $signature);
+
+            case 'openssl':
+                $result = openssl_verify($input, $signature, $key, $algo);
+                if ($result === -1) {
+                    throw new Exception('OpenSSL-Fehler: ' . openssl_error_string());
+                }
+                return $result === 1;
+
+            default:
+                throw new Exception('Nicht unterstützte Verifikationsfunktion');
+        }
+    }
+
+    /**
      * Erzeugt eine Base64Url-Kodierung
      *
      * @param string $data Zu kodierende Daten
